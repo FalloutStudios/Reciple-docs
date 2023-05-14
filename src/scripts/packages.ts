c59fcfb50d2467ec045f6a1656af264bc82a8794
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-05-14 14:17:12 +0800

    Add npm-loader

--- src/scripts/packages.ts
+++ src/scripts/packages.ts
@@ -1,7 +1,9 @@
 import Client from '../data/Client';
+import NPMLoader from '../data/NPMLoader';
 import Reciple from '../data/Reciple';
 
 export default {
     'reciple': Reciple,
-    'client': Client
+    'client': Client,
+    'npm-loader': NPMLoader
 };
