a4436b206e3b4d8f00e7eff97489f5b26753848e
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-05-23 21:49:57 +0800

    e

--- src/scripts/packages.ts
+++ src/scripts/packages.ts
@@ -1,9 +1,11 @@
 import Client from '../data/Client';
 import NPMLoader from '../data/NPMLoader';
 import Reciple from '../data/Reciple';
+import UpdateChecker from '../data/UpdateChecker';
 
 export default {
     'reciple': Reciple,
     'client': Client,
-    'npm-loader': NPMLoader
+    'npm-loader': NPMLoader,
+    'update-checker': UpdateChecker
 };
