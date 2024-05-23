98d3df513b05395c9a6efc6e328060f5e579656d
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-05-23 22:54:05 +0800

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
