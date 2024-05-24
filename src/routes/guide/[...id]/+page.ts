990597a221dc584d66662cb1e0894eadcdc71799
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-05-24 13:14:28 +0800

    redirect

--- src/routes/guide/[...id]/+page.ts
+++ src/routes/guide/[...id]/+page.ts
@@ -1,5 +1,17 @@
+import { error } from '@sveltejs/kit';
+import type { Guides } from '../../api/guides/+server.js';
+
 export async function load(data) {
-    const [category, id] = data.params.id.split('/');
+    let [category, id] = data.params.id.split('/') as (string|undefined)[];
+
+    if (!category || !id) {
+        const guides = await (await data.fetch('/api/guides')).json() as Guides;
+
+        category ??= guides[0].category;
+        id ??= guides.find(c => c.category === category)?.pages[0].id;
+    }
+
+    if (!category || !id) return error(404);
 
     const markdown = await import(`../../../guides/${category}/${id}.svx`);
 
