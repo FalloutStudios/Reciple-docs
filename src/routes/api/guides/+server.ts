f4c5f62d30ecda40aa29234ae0131ec65f35523a
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-05-24 09:58:15 +0800

    e

--- src/routes/api/guides/+server.ts
+++ src/routes/api/guides/+server.ts
@@ -1,13 +1,12 @@
 import { readdir } from 'fs/promises';
 import path from 'path';
-import { searchForWorkspaceRoot } from 'vite';
 import { parseGuideId } from '$lib/scripts/guideHelpers';
 import { json } from '@sveltejs/kit';
 
 export type Guides = { category: string; pages: string[]; }[];
 
 export async function GET() {
-    const basename = path.join(searchForWorkspaceRoot(process.cwd()), './src/guides');
+    const basename = path.join(process.cwd(), './src/guides');
     const folders = await readdir(basename);
 
     return json(await Promise.all(folders.map(async f => ({
