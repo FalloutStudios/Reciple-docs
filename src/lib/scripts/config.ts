4361cb91c602341c634bd225f13acf76c005a05a
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-05-27 15:38:25 +0800

    change nav links

--- src/lib/scripts/config.ts
+++ src/lib/scripts/config.ts
@@ -9,17 +9,16 @@ export const npmHome = 'https://npmjs.com/package/reciple';
 
 export const navLinks: { name: string; href: string; blank?: boolean; }[] = [
     {
-        name: 'Reciple',
-        href: '/'
+        name: 'Guide',
+        href: '/guide'
+    },
+    {
+        name: 'Docs',
+        href: '/docs',
     },
     {
         name: 'GitHub',
         href: githubHome,
-        blank: true
-    },
-    {
-        name: 'Discord.js',
-        href: 'https://discordjs.dev',
         blank: true
     }
 ];
